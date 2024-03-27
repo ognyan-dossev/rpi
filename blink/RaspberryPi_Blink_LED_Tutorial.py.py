f14117115e517fc406cdbfa86b3f44d70956227a
ognyan-dossev <ognyan.dossev@gmail.com>
2024-03-27 19:29:34 +0200

    blink count

--- blink/RaspberryPi_Blink_LED_Tutorial.py.py
+++ blink/RaspberryPi_Blink_LED_Tutorial.py.py
@@ -4,10 +4,12 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18,GPIO.OUT)
-while True:
+count=30
+while count > 0:
     GPIO.output(18,GPIO.HIGH)
     print ("To LED HIGH")
     sleep(1)
     GPIO.output(18,GPIO.LOW)
     print ("To LED LOW")
     sleep(1)
+    count=count-1
